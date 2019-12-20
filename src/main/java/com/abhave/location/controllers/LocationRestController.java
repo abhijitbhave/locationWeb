eb1ed341d9f739b23f2fff96e89a14c9714cd130
Abhijit Bhave <abhijit.bhave@gmail.com>
2019-12-20 09:43:07 -0800

    new changes

--- src/main/java/com/abhave/location/controllers/LocationRestController.java
+++ src/main/java/com/abhave/location/controllers/LocationRestController.java
@@ -3,11 +3,10 @@ package com.abhave.location.controllers;
 import com.abhave.location.entities.Location;
 import com.abhave.location.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/locations")
@@ -19,5 +18,26 @@ public class LocationRestController {
     @GetMapping
     public List<Location> getLocations() {
         return locationRepository.findAll();
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteLocation(@PathVariable("id") int id) {
+        locationRepository.deleteById(id);
+    }
+
+    @GetMapping("{id}")
+    public Location getLocation(@PathVariable("id") int id) {
+        Optional<Location> optionalLocation = locationRepository.findById(id);
+        return optionalLocation.get();
+
     }
 }
